f01cd3d6a2fc16dbe71f8a7ec60a60421dbc4d13
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-11-19 00:02:33 +0300

    Создан файл app.py.
    Позволяет на минималках запустить приложение

---
+++ src/backend/app.py
@@ -0,0 +1,8 @@
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+from core.config import settings
+
+load_dotenv()
+
+app = FastAPI(title=settings.app_title, description=settings.app_description, version=settings.app_version)
