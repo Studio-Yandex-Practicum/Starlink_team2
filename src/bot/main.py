d158be1aea3eb7a51b382320b549f2ffd21eaa2f
Войцик Иван <voitsik.ivan@yandex.ru>
2024-11-19 21:29:25 +0300

    Добавление бота и переменных окружения

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,6 +1,15 @@
 # Это основной запускаемый файл.
 # Заменить содержимое своим кодом.
+import telebot
+import os
 
-import sys
+from dotenv import load_dotenv
 
-print(sys.path)
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+bot = telebot.TeleBot(BOT_TOKEN)
+
+if __name__ == '__main__':
+    bot.polling()
