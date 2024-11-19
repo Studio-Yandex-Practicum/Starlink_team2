622066e85f92092ab99e7248822ab7f251fc8890
Валерий <94367062+gugutsidze-vv@users.noreply.github.com>
2024-11-19 21:17:56 +0300

    Добавлены креды для подключения к БД.

--- src/backend/core/config.py
+++ src/backend/core/config.py
@@ -8,6 +8,10 @@ class Settings(BaseSettings):
     app_title: str
     app_description: str
     app_version: str
+    postgres_user: str
+    postgres_password: str
+    postgres_host: str
+    postgres_db_name: str
 
 
 settings = Settings()
