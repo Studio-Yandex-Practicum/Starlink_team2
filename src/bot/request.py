0897eadc88e827608750a7efe1aeeb6221d91b45
Ivan School <pugovizza@gmail.com>
2024-11-21 12:33:22 +0300

    fiks session

--- src/bot/request.py
+++ src/bot/request.py
@@ -1,16 +1,15 @@
-from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from telebot.types import Message
-
+#from database import async_session
 #from DB.model import TelegramUser, Role, Email
 
 
 async def start_chek_user(
         message: Message,
-        session: AsyncSession,
 ) -> TelegramUser:
     """Проверка пользователя."""
-    user_bd = await session.query(TelegramUser).get(message.from_user.id)
+    async with async_session() as session:
+        user_bd = await session.query(TelegramUser).get(message.from_user.id)
     if user_bd:
         await chek_user_email(user_bd)
         return user_bd
@@ -19,26 +18,25 @@ async def start_chek_user(
 
 async def chek_user_email(
         user_bd: TelegramUser,
-        session: AsyncSession,
 ) -> None:
     """Проверка почты в списке сотрудников.
 
     :param user_bd: объект TelegramUser.
     """
-    bd_email = await session.query(Email).get(bd_email.email)
-    if not(bd_email):
-        user_bd.role = 0
-        await session.refresh(user_bd)
+    async with async_session() as session:
+        bd_email = await session.query(Email).get(bd_email.email)
+        if not(bd_email):
+            user_bd.role = 0
+            await session.refresh(user_bd)
+            await session.commit()
 
 
 async def add_user(
         message: Message,
-        session: AsyncSession,
 ) -> TelegramUser:
     """Добавление пользователя в БД.
 
     :param message: объект сообщение из Telegram.
-    :param session: мейкер асинхронной сессии.
     """
     new_user = TelegramUser(
             UniqueID=message.from_user.id,
@@ -49,5 +47,7 @@ async def add_user(
             Email=0,
             Active=True,
         )
-    session.add(new_user)
+    async with async_session() as session:
+        session.add(TelegramUser(new_user))
+        await session.commit()
     return new_user
