930c73bf2e2cb7c51bdd15f411b4f01d90d09afe
Vladimir Molchanov <r1ken@mail.ru>
2024-11-21 21:08:44 +0300

    Изменил core/base, добавил модели в models/__init__

--- src/backend/core/base.py
+++ src/backend/core/base.py
@@ -1,7 +1,1 @@
 from core.db import Base  # noqa
-from models.menu import Menu  # noqa
-from models.quiz import Quiz  # noqa
-from models.user_quizes import UserQuize  # noqa
-from models.email import Email  # noqa
-from models.role import Role  # noqa
-from models.telegram_user import TelegramUser  # noqa
