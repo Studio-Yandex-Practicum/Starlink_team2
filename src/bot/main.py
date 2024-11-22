17736455288ecdb48e8ff1f148f83918d4435eba
Ivan School <pugovizza@gmail.com>
2024-11-22 16:19:34 +0300

    fix async bot

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,5 +1,6 @@
+import asyncio
+
 from commands import *  # noqa: F403
 from loader import bot
 
-if __name__ == '__main__':
-    bot.polling(none_stop=True)
+asyncio.run(bot.polling(skip_pending=True))
