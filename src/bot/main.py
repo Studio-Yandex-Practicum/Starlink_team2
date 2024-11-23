2ea5f4429d8a15d3d3943176dfc35f3f84210084
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-11-23 23:44:20 +0300

    Добавил логгирование при запуске.

--- src/bot/main.py
+++ src/bot/main.py
@@ -1,6 +1,26 @@
 import asyncio
+import logging
 
-from commands import *  # noqa: F403
+import handlers  # noqa
 from loader import bot
+from utils.logger import get_logger
 
-asyncio.run(bot.polling(skip_pending=True))
+
+async def main() -> None:
+    """Запуск оболочки бота."""
+    log = get_logger(__name__)
+    try:
+        log.info('Bot started')
+        await bot.infinity_polling(
+            skip_pending=True,
+            logger_level=logging.INFO,
+        )
+        log.info('Bot stopped')
+    except Exception as e:
+        log.info(e)
+
+
+if __name__ == '__main__':
+    asyncio.run(
+        main(),
+    )
