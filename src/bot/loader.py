51bb044822db8abaf5483243a611ed35903ef520
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-11-24 00:08:17 +0300

    Исправлены ошибки

--- src/bot/loader.py
+++ src/bot/loader.py
@@ -4,7 +4,6 @@ from typing import Optional
 from dotenv import load_dotenv
 from telebot.async_telebot import AsyncTeleBot
 
-
 load_dotenv()
 BOT_TOKEN: Optional[str] = os.getenv("BOT_TOKEN")
 if not BOT_TOKEN:
