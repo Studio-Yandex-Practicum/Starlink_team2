e9e6309a4d82b5e765f7c9b353970035f6c40a3f
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-11-23 23:48:12 +0300

    Проверка на существование BOT_TOKEN

--- src/bot/loader.py
+++ src/bot/loader.py
@@ -1,9 +1,13 @@
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 from telebot.async_telebot import AsyncTeleBot
 
-load_dotenv()
-BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-bot = AsyncTeleBot(BOT_TOKEN)
+load_dotenv()
+BOT_TOKEN: Optional[str] = os.getenv("BOT_TOKEN")
+if not BOT_TOKEN:
+    raise ValueError("BOT_TOKEN is not set")
+else:
+    bot = AsyncTeleBot(BOT_TOKEN)
