6843f62237f547551f2be76bfbb97184804435f3
flyde2 <flypolyris@yandex.ru>
2024-11-29 14:47:57 +0300

    lifespan

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,5 +1,6 @@
-
 import os
+from contextlib import asynccontextmanager
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
 from passlib.handlers.sha2_crypt import sha512_crypt as crypto
@@ -13,41 +14,44 @@ from backend.pages.pages import router
 from backend.models.admin import Admin
 from backend.core.config import settings
 
-
 load_dotenv()
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Lifespan context manager для инициализации данных.
+    """
+    async with get_async_session() as session:
+        try:
+            result = await session.execute(select(Admin))
+            user = result.scalars().first()
+            if not user:
+                user1 = Admin(username=os.getenv("ADMIN_USER1_USERNAME"),
+                              hashed_password=crypto.hash(
+                                  os.getenv("ADMIN_USER1_PASSWORD"))
+                              )
+                user2 = Admin(username=os.getenv("ADMIN_USER2_USERNAME"),
+                              hashed_password=crypto.hash(
+                                  os.getenv("ADMIN_USER2_PASSWORD"))
+                              )
+                session.add_all([user1, user2])
+                await session.commit()
+                print("Администраторы успешно созданы.")
+        except SQLAlchemyError as e:
+            print(f"Database error: {e}")
+    yield
 
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.app_description,
     version=settings.app_version,
+    lifespan=lifespan
 )
 
-
 app.include_router(router)
-
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 static_dir = os.path.join(base_dir, 'static')
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
-
-@app.on_event("startup")
-async def startup():
-    """
-    Создание первых пользователей.
-    """
-    async with get_async_session() as session:
-        try:
-            statement = select(Admin)
-            result = await session.execute(statement)
-            user = result.scalars().first()
-            if not user:
-                user1 = Admin(username="user1@gmail.com",
-                              hashed_password=crypto.hash("12345"))
-                user2 = Admin(username="user2@gmail.com",
-                              hashed_password=crypto.hash("54321"))
-                session.add_all([user1, user2])
-                await session.commit()
-        except SQLAlchemyError as e:
-            print(f"Database error: {e}")
