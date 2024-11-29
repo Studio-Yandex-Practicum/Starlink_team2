edbe61303e3c95c393cabb238016db6e9586fc61
Nikolay Klepov <149668988+flyde2@users.noreply.github.com>
2024-11-29 22:16:39 +0300

    Fastapi lifespan (#19)
    * lifespan
    
    * lifespan
    
    * lifespan

--- src/backend/app.py
+++ src/backend/app.py
@@ -1,53 +1,54 @@
-
+from contextlib import asynccontextmanager
 import os
+
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from passlib.handlers.sha2_crypt import sha512_crypt as crypto
-from rich import print
 from fastapi.staticfiles import StaticFiles
+from passlib.handlers.sha2_crypt import sha512_crypt as crypto
+from rich import print as rich_print
 from sqlalchemy import select
 from sqlalchemy.exc import SQLAlchemyError
 
-from backend.core.db import get_async_session
-from backend.pages.pages import router
-from backend.models.admin import Admin
 from backend.core.config import settings
-
+from backend.core.db import get_async_session
+from backend.models.admin import Admin
+from backend.pages.pages import router
 
 load_dotenv()
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """Lifespan context manager для инициализации данных."""
+    async with get_async_session() as session:
+        try:
+            result = await session.execute(select(Admin))
+            user = result.scalars().first()
+            if not user:
+                user1 = Admin(username=os.getenv("ADMIN_USER1_USERNAME"),
+                              hashed_password=crypto.hash(
+                                  os.getenv("ADMIN_USER1_PASSWORD")),
+                              )
+                user2 = Admin(username=os.getenv("ADMIN_USER2_USERNAME"),
+                              hashed_password=crypto.hash(
+                                  os.getenv("ADMIN_USER2_PASSWORD")),
+                              )
+                session.add_all([user1, user2])
+                await session.commit()
+                rich_print("[green]Администраторы успешно созданы.[/green]")
+        except SQLAlchemyError as e:
+            rich_print(f"[red]Database error: {e}[/red]")
+    yield
 
 
 app = FastAPI(
     title=settings.app_title,
     description=settings.app_description,
     version=settings.app_version,
+    lifespan=lifespan,
 )
 
-
 app.include_router(router)
-
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 static_dir = os.path.join(base_dir, 'static')
 app.mount("/static", StaticFiles(directory=static_dir), name="static")
-
-
-@app.on_event("startup")
-async def startup():
-    """
-    Создание первых пользователей.
-    """
-    async with get_async_session() as session:
-        try:
-            statement = select(Admin)
-            result = await session.execute(statement)
-            user = result.scalars().first()
-            if not user:
-                user1 = Admin(username="user1@gmail.com",
-                              hashed_password=crypto.hash("12345"))
-                user2 = Admin(username="user2@gmail.com",
-                              hashed_password=crypto.hash("54321"))
-                session.add_all([user1, user2])
-                await session.commit()
-        except SQLAlchemyError as e:
-            print(f"Database error: {e}")
