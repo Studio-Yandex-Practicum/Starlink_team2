c3d7f1cb7d3c3a61016f8eb40d73ba5be86ddc3e
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-12-01 19:53:42 +0300

    Add check for migration.

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,4 +1,9 @@
-alembic revision --autogenerate -m "Intial migration"
+if alembic check | grep -q 'No new upgrade operations detected.'; then
+    echo 'No migrations detected'
+else
+    alembic revision --autogenerate -m 'Intial migration'
+fi
+
 alembic upgrade head
 
 python main.py
