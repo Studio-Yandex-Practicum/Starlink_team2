c45b857d964e181badac3d765cb47fa5ea4790f2
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2024-12-05 00:15:50 +0300

    Ruff anotation fix.

--- src/bot/crud/fill_db.py
+++ src/bot/crud/fill_db.py
@@ -1,17 +1,23 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker
 
-from backend.models import Role
+from backend.models import EmployeeEmail, Menu, Role
 from bot.db import async_session
 
 
 class CreateDataInDB:
-    def __init__(self, model) -> None:
+    """Класс для заполнения базы данных."""
+
+    def __init__(self, model: EmployeeEmail | Menu | Role) -> None:
+        """Инициализация класса."""
         self.model = model
 
     async def fill_db_from_json(
-        self, data: dict, session: async_sessionmaker[AsyncSession],
-    ):
+        self,
+        data: dict,
+        session: async_sessionmaker[AsyncSession],
+    ) -> EmployeeEmail | Menu | Role:
+        """Заполнение базы данных."""
         async with session() as asession:
             new_data = self.model(**data)
             asession.add(new_data)
@@ -19,14 +25,20 @@ class CreateDataInDB:
             return new_data
 
     async def check_db_is_empty(
-        self, session: async_sessionmaker[AsyncSession],
-    ):
+        self,
+        session: async_sessionmaker[AsyncSession],
+    ) -> list | None:
+        """Проверка базы данных на наличие данных."""
         async with session() as asession:
             db_objs = await asession.execute(select(self.model))
             return db_objs.scalars().first() if db_objs else None
 
 
-async def create_data_in_db(model, data) -> str:
+async def create_data_in_db(
+    model: EmployeeEmail | Menu | Role,
+    data: list[dict],
+) -> str:
+    """Заполнение базы данных."""
     create_crud = CreateDataInDB(model)
     check_db = await create_crud.check_db_is_empty(session=async_session)
     if check_db is not None:
@@ -34,7 +46,8 @@ async def create_data_in_db(model, data) -> str:
     else:
         for item in data:
             await create_crud.fill_db_from_json(
-                data=item, session=async_session,
+                data=item,
+                session=async_session,
             )
         message_to_send = 'База данных УСПЕШНО заполнена'
     return message_to_send
