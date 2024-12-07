ba00a1cfe63bc600dac28f6328aa3d97c3654a69
gugutsidze-vv <gugutsidze-vv@rosakhutor.com>
2024-12-07 15:11:51 +0300

    Добавлен функционал для роли.
    Добавлено поле для обозначения роли, которая дается после авторизации.
    src/backend/crud/role.py - добавлен метод `get_minimal_role`, который получает минимальную роль для пользователя.
    src/backend/pages/roles.py - изменен метод `role_edit` для проверки уникальности минимальной роли в БД
    src/backend/schemas/role.py - изменена схема обновления.

--- src/backend/crud/role.py
+++ src/backend/crud/role.py
@@ -1,3 +1,8 @@
+from typing import Optional
+
+from sqlalchemy import select
+
+from backend.core.db import get_async_session
 from backend.crud.base import CRUDBase
 from backend.models import Role
 
@@ -5,7 +10,15 @@ from backend.models import Role
 class CRUDRole(CRUDBase):
     """CRUD для работы с моделью Role."""
 
-    pass
+    async def get_minimal_role(self) -> Optional[Role]:
+        """Получить минимальную роль."""
+        async with get_async_session() as session:
+            item = await session.execute(
+                select(self.model).filter(
+                    self.model.default_minimal_role.is_(True),
+                ),
+            )
+        return item.scalars().first()
 
 
 role_crud = CRUDRole(Role)
