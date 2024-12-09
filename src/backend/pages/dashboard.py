ce234670e9846c61ab9ee585b52faa82f8decf26
Kirill <ka-kuznetsov@yandex.ru>
2024-12-09 16:19:02 +0300

    Разработана главная страница приложения с отображением информации, так же с графиком на js общено количества пользователей,  пользователей за вчерашний и за сегоднешний день. Отображение последних добавленых пользователей - 10 записей на страницую. Поправлен общий шаблон.

--- src/backend/pages/dashboard.py
+++ src/backend/pages/dashboard.py
@@ -3,12 +3,15 @@ import os
 from fastapi import APIRouter, Depends, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from backend.crud import role_crud, telegramuser_crud
+from backend.models import Admin
+from datetime import datetime, timedelta
+from datetime import date
 
 from backend.core.auth import (
     get_current_user_from_cookie,
     get_current_user_from_token,
 )
-from backend.models.admin import Admin
 
 router = APIRouter()
 
@@ -24,20 +27,41 @@ async def dashboard(
 ) -> HTMLResponse:
     """Обрабатывает запрос на страницу управления ботом.
 
+    Функция получает информацию о пользователях Telegram,
+        ролях и статистике регистрации
+    для их последующего отображения на панели управления.
+
     Args:
-        request: Объект запроса.
-        user: Текущий пользователь (извлекается из токена).
+        request (Request): Объект запроса от клиента.
+        user (Admin, optional): Текущий пользователь, извлекаемый из токена.
+            По умолчанию равен None.
 
     Returns:
-        HTML-ответом с контекстом страницы.
-
+        HTMLResponse: HTML-ответ с контекстом страницы,
+            включающим информацию о пользователях,
+                ролях и статистике регистрации.
     """
     try:
         user = await get_current_user_from_cookie(request)
     except Exception as _:
         user = None
+    tgusers = await telegramuser_crud.get_multi()
+    yesterday = datetime.now() - timedelta(days=1)
+    tgusers_yesterday = [
+        user for user in tgusers if user.created_at > yesterday
+    ]
+    today = date.today()
+    tgusers_today = [
+        user for user in tgusers if user.created_at.date() == today
+    ]
+    roles = await role_crud.get_multi()
     context = {
-        'user': user,
-        'request': request,
+        "user": user,
+        "request": request,
+        "tgusers": tgusers,
+        "roles": roles,
+        "tgusers_yesterday": tgusers_yesterday,
+        "tgusers_today": tgusers_today,
+
     }
     return templates.TemplateResponse('/dashboard.html', context)
